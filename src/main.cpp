ad8287b9744718f5444b81b645998c7863cb6e9e
Mateuszszszszsz <133503123+Mateuszszszszsz@users.noreply.github.com>
2026-07-18 13:45:33 +0200

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -6,73 +6,69 @@
 #include "TheaterChase.h"
 #include "Arduino.h"
 
-
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
 
+#define BUTTON 4
+const unsigned long DEBOUNCE_DELAY_MS = 50;
+
+bool lastButtonState = HIGH;
+bool stableButtonState = HIGH;
+unsigned long lastDebounceTime = 0;
+
+void toggleColor(Adafruit_NeoPixel& strip, uint32_t color1, uint32_t color2) {
+  static bool isColor1 = true;
+
+  if (isColor1) {
+    colorWipe(strip, color1);
+  } else {
+    colorWipe(strip, color2);
+  }
+
+  isColor1 = !isColor1;
+} 
+
 void setup() {
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-  #if defined (__AVR_ATtiny85__)
-    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-  #endif
+#if defined(__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
   // End of trinket special code
 
+  pinMode(BUTTON, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
+
   strip.begin();
-  strip.setBrightness(40);
+  strip.setBrightness(20);
   strip.show(); // Initialize all pixels to 'off'
   Serial.begin(9600);
-  
+
+  colorWipe(strip, WHITE);
 }
 
-void loop() 
-{
-int i = 0;
-  while(1)
-  { 
+void loop() {
+  int reading = digitalRead(BUTTON);
 
- // rainbow(strip,30);
-  //rainbowheart(strip, 30,0);
-  //rainbowCycle(strip,30);
-  //rainbowheart(strip, 30,1);
-  int aDel = 1;
-  aquatronic(strip,aDel,8);
-  aquatronic(strip,aDel,6);
-  rainbowheart(strip,aDel,0);
-  aquatronic(strip,aDel,0);
-  aquatronic(strip,aDel,1);
-  aquatronic(strip,aDel,2);
-  aquatronic(strip,aDel,0);
-  aquatronic(strip,aDel,3);
-  aquatronic(strip,aDel,4);
-  aquatronic(strip,aDel,5);
-  aquatronic(strip,aDel,6);
-  aquatronic(strip,aDel,7);
-  aquatronic(strip,aDel,8);
-  rainbowheart(strip,2*aDel,0);    
-//  theaterChase(strip,WHITE,100);
-  
-  i++;
-  i= i%256;
-  delay(100);
-  //Serial.println(i);
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+    lastButtonState = reading;
   }
+
+  if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY_MS) {
+    if (reading != stableButtonState) {
+      stableButtonState = reading;
+
+      if (stableButtonState == LOW) {
+        Serial.println("Button pressed");
+        toggleColor(strip, RED, WHITE);
+      } else {
+        Serial.println("Button released");
+      }
+    }
+  }
+
+  digitalWrite(LED_BUILTIN, stableButtonState == LOW ? HIGH : LOW);
 }
 
 
-
-// // Wczytywanie danej z klawiatury po UART
-// if(Serial.available()>0)
-// {
-// int width = Serial.parseInt();
-// Serial.print("otrzymalem:");
-// Serial.println(width);
-// strip.setBrightness(width);
-// }
-
-void function1(Adafruit_NeoPixel& strip, uint32_t& color, uint8_t wait = 10) //strobo przelatujące binarnie po kolorach dobrać szybkość zmian (wait jako prescaler albo zmieniać wartość incrementu ) 
-{
-  colorWipe(strip, color, wait);
-  strip.show();
-  color+=10000;
-}
